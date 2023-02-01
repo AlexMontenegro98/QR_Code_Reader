9a75801656ee96bb57196078f51bb47d7162b26b
AlexMontenegro98 <69175709+AlexMontenegro98@users.noreply.github.com>
2023-02-01 17:55:21 -0300

    Add files via upload

---
+++ reader.py
@@ -0,0 +1,14 @@
+import cv2
+import streamlit as st
+from pyzbar.pyzbar import decode
+import numpy as np
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+    bytes_data = picture.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    for i in decode(cv2_img):
+        a = i.data.decode('utf-8')
+        st.write(a)
