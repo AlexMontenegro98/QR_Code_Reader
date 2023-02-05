0cf01cab1ea403a7d1a68eb58ada24880c827f62
AlexMontenegro98 <69175709+AlexMontenegro98@users.noreply.github.com>
2023-02-04 22:54:25 -0300

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,14 +1,25 @@
-import cv2
 import streamlit as st
-from pyzbar.pyzbar import decode
+import cv2
 import numpy as np
 
-picture = st.camera_input("Take a picture")
+def decode(im) : 
+    # Find QR code
+    decodedObjects = cv2.QRCodeDetector().detectAndDecode(im)
+    
+    # If there's a QR code in the image, display the decoded message
+    if decodedObjects[0] :
+        print("QR code detected, data:", decodedObjects[1])
+        st.write("QR code detected, data:", decodedObjects[1])
+    else:
+        print("QR code not detected")
+        st.write("QR code not detected")
 
+# Load image
+#im = cv2.imread(r"C:\Python\Ready_Codes\nota_fiscal\notafis.jpg")
+picture = st.camera_input("Take a picture")
 if picture:
     bytes_data = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    for i in decode(cv2_img):
-        a = i.data.decode('utf-8')
-        st.write(a)
+# Decode QR code
+decode(cv2_img)
